8a540d9b55a083fe67d5f915c612000ec9410c76
Julian Carlosama <cromocelcarlosama@gmail.com>
2021-08-08 13:16:54 -0500

    New function

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -1,18 +1,44 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
- * display_complex_number - Function that show complex numbers
- *
- * @complex: Get the structure.
+ * display_complex_number - Function that show
+ * complex numbers
  *
  * @c: imaginary numbers.
  *
+ * Return: No return.
  */
 
 void display_complex_number(complex c)
 {
 	if (c.re == 0 && c.im == 0)
-		return (0);
+		return;
 	else if (c.re != 0 && c.im != 0)
-		printf("%0.f + %0.f", c.re, c.im);
+	{
+		if (c.re > 0 && c.im > 0)
+		{
+			if (c.im == 1)
+				printf("%0.f + i\n", c.re);
+			else
+				printf("%0.f + %0.fi\n", c.re, c.im);
+		}
+		else if (c.re > 0 && c.im < 0)
+			printf("%0.f - %0.fi\n", c.re, (-1) * c.im);
+		else if (c.re < 0 && c.im > 0)
+			printf("-%0.f + %0.fi\n", (-1) *  c.re, c.im);
+		else if (c.re < 0 && c.im < 0)
+			printf("-%0.f - %0.fi\n", (-1) * c.re,  (-1) * c.im)
+	}
+	else if (c.re == 0 || c.im == 0)
+	{
+		if (c.re > 0)
+			printf("%0.f\n", c.re);
+		else if (c.im > 0)
+			printf("%0.fi\n", c.im);
+		else if (c.im < 0)
+			printf("%0.fi\n", c.im);
+		else if (c.re < 0)
+			printf("%0.f\n", c.re);
+	}
 }
